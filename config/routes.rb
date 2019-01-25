f605a6aee01c719d4f33772da2a96528402d79f7
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2019-01-25 18:06:19 -0200

    Commit Initial

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: 'pages#home'  
+  resources :posts
+  resources :categories
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
